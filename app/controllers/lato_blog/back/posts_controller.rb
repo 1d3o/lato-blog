95c3d49e222fed57f9336dd4f309e28356203de3
Gregorio Galante <gregogalante@gmail.com>
2017-05-09 16:01:48 +0200

    set base settings for posts

--- app/controllers/lato_blog/back/posts_controller.rb
+++ app/controllers/lato_blog/back/posts_controller.rb
@@ -1,6 +1,10 @@
 module LatoBlog
   class Back::PostsController < Back::BackController
 
+    before_action do
+      core__set_menu_active_item('blog_articles')
+    end
+
     def index
 
     end
--- db/migrate/20170504223521_create_lato_blog_post_parents.rb
+++ db/migrate/20170504223521_create_lato_blog_post_parents.rb
@@ -1,7 +1,8 @@
 class CreateLatoBlogPostParents < ActiveRecord::Migration[5.1]
   def change
     create_table :lato_blog_post_parents do |t|
-
+      t.datetime :publication_datetime
+      
       t.timestamps
     end
   end
