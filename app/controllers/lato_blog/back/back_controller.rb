1649cbe91ca8d1da69a17b7269af3d1c8d563c05
Gregorio Galante <gregogalante@gmail.com>
2017-05-10 09:44:50 +0200

    make switch language dynamic

--- app/controllers/lato_blog/back/back_controller.rb
+++ app/controllers/lato_blog/back/back_controller.rb
@@ -3,7 +3,31 @@ module LatoBlog
 
     layout 'lato_core/admin'
 
+    # check user is logged
     before_action :core__manage_superuser_session
+    # set default language if not exist
+    before_action :set_default_current_language
+
+    # This function switch the default language used by the system and redirect to the same user page.
+    def switch_current_language
+      languages = blog__get_languages_identifier
+      if languages.include? params[:language]
+        cookies[:lato_blog__current_language] = params[:language]
+      elsif languages && languages.length > 0
+        cookies[:lato_blog__current_language] = languages.first 
+      end
+      
+      redirect_to lato_blog.root_path
+    end
+
+    private
+
+      def set_default_current_language
+        if !cookies[:lato_blog__current_language]
+          languages = blog__get_languages_identifier
+          cookies[:lato_blog__current_language] = (languages && languages.length > 0) ? languages.first : nil
+        end
+      end
     
   end
 end
--- config/routes/back.rb
+++ config/routes/back.rb
@@ -1,2 +1,5 @@
+# main routes
+get 'switch_current_language', to: 'back/back#switch_current_language', as: 'switch_current_language'
+
 # posts 
 resources :posts, module: 'back'
