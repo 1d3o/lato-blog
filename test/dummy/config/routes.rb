c96f6f19514bf0b3b60fbe0dc228b958cc32b95c
Gregorio Galante <gregogalante@gmail.com>
2017-05-13 09:57:18 +0200

    remove tests

--- test/dummy/config/routes.rb
+++
@@ -1,3 +0,0 @@
-Rails.application.routes.draw do
-  mount LatoBlog::Engine => "/lato_blog"
-end
--- test/lato_blog_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class LatoBlog::Test < ActiveSupport::TestCase
-  test "truth" do
-    assert_kind_of Module, LatoBlog
-  end
-end
