2ecfc5f07eae3d4119e1e7acfae11d8e12577d6e
Gregorio Galante <gregogalante@gmail.com>
2017-05-16 09:30:27 +0200

    init index categories view

--- app/controllers/lato_blog/back/categories_controller.rb
+++ app/controllers/lato_blog/back/categories_controller.rb
@@ -7,7 +7,15 @@ module LatoBlog
 
     # This function shows the list of possible categories.
     def index
+      core__set_header_active_page_title(LANGUAGES[:lato_blog][:pages][:categories])
+      @categories = LatoBlog::Category.all
+      @widget_index_categories = core__widgets_index(@categories, pagination: 10)
+    end
 
+    # This function shows a single category. It create a redirect to the edit path.
+    def show
+      # use edit as default post show page
+      redirect_to lato_blog.edit_category_path(params[:id])
     end
     
   end
