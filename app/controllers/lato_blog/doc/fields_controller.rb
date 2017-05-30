da2aa26e28386f6b653a14fdd1cb51b2ab854a97
Gregorio Galante <gregogalante@gmail.com>
2017-05-30 18:12:25 +0200

    add text documentation

---
+++ app/controllers/lato_blog/doc/fields_controller.rb
@@ -0,0 +1,7 @@
+module LatoBlog
+  class Doc::FieldsController < Doc::DocController
+
+    def text; end
+
+  end
+end
--- config/routes/doc.rb
+++ config/routes/doc.rb
@@ -2,4 +2,7 @@ namespace :doc do
 
   root 'doc#index'
 
+  # fields
+  get 'fields_text', to: 'fields#text', as: 'fields_text'
+
 end
