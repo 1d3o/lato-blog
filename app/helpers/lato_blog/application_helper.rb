1e6b2f45a80c320c2a9161f8f96f903296eb8bda
Gregorio Galante <gregogalante@gmail.com>
2017-05-30 17:30:25 +0200

    init fields render

--- app/helpers/lato_blog/application_helper.rb
+++ app/helpers/lato_blog/application_helper.rb
@@ -7,6 +7,19 @@ module LatoBlog
         return language[:title] if language[:identifier] === cookies[:lato_blog__current_language]
       end
     end
-    
+
+    # This function render a single custom post field.
+    def render_post_field(post, key)
+      post_field = post.post_fields.find_by(key: key)
+      return unless post_field
+
+      case post_field.typology
+      when 'text'
+        render 'lato_blog/back/posts/shared/fields/text', field: post_field
+      else
+        return
+      end
+    end
+
   end
 end
