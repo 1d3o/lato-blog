998c62247995e0456d1ed8d667bf9fb21f8ebab5
Gregorio Galante <gregogalante@gmail.com>
2017-06-08 14:56:04 +0200

    complete api

--- app/models/lato_blog/post_field/serializer_helpers.rb
+++ app/models/lato_blog/post_field/serializer_helpers.rb
@@ -18,11 +18,31 @@ module LatoBlog
     # This function returns the correct serialized value of the field.
     def serialize_value
       case typology
-      when 'TODO'
-        nil
+      when 'media'
+        serialize_value_media
       else
         value
       end
+    end
+
+    # This function serialize a media value and return it.
+    def serialize_value_media
+      media = LatoMedia::Media.find_by(id: value)
+      return unless media
+
+      # add basc info
+      serialized = {}
+      serialized[:id] = media.id
+      serialized[:title] = media.title
+      serialized[:url] = media.attachment.url
+
+      # add image info
+      serialized[:thumb_url] = (media.image? ? media.attachment.url(:thumb) : nil)
+      serialized[:medium_url] = (media.image? ? media.attachment.url(:medium) : nil)
+      serialized[:large_url] = (media.image? ? media.attachment.url(:large) : nil)
+
+      # return serialized media
+      serialized
     end
 
   end
