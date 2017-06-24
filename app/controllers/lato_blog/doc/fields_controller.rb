b02a9bedd8e75f68028c7e2b6742fee15df1c918
Gregorio Galante <gregogalante@gmail.com>
2017-06-24 19:47:18 +0200

    complete clean of old fields

--- app/controllers/lato_blog/doc/fields_controller.rb
+++ app/controllers/lato_blog/doc/fields_controller.rb
@@ -1,11 +1,5 @@
 module LatoBlog
   class Doc::FieldsController < Doc::DocController
 
-    def text; end
-
-    def media; end
-
-    def geolocalization; end
-
   end
 end
--- config/routes/doc.rb
+++ config/routes/doc.rb
@@ -2,10 +2,4 @@ namespace :doc do
 
   root 'doc#index'
 
-  # fields
-  get 'fields_text', to: 'fields#text', as: 'fields_text'
-  get 'fields_media', to: 'fields#media', as: 'fields_media'
-  get 'fields_geolocalization', to: 'fields#geolocalization', as: 'fields_geolocalization'
-
-
 end
