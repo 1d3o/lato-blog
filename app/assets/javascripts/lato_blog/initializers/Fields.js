6cd0ef1ee98914679e1c6662149a991e9e3a644d
Gregorio Galante <gregogalante@gmail.com>
2017-06-29 20:02:19 +0200

    enable autosaving when play with relay

--- app/assets/javascripts/lato_blog/initializers/Fields.js
+++ app/assets/javascripts/lato_blog/initializers/Fields.js
@@ -2,6 +2,9 @@ var BlogFieldsInitializer = (function () {
 
   function initializeRelayNewButton () {
     $('.fields-relay__menu-add').click(function (e) {
+      // save current status with form submit
+      _relaySubmitUpdateBeforeAction()
+      // open - close menu
       var parent = $(this).parent()
       var menu = $(parent).find('.fields-relay__menu')
       $(menu).toggleClass('fields-relay__menu--active')
@@ -9,26 +12,40 @@ var BlogFieldsInitializer = (function () {
     })
   }
 
-  function appendDestroy() {
-    $('.js-sortable__main-container').each(function(i, el) {
-      $(el).find('.js-sortable__element').each(function(index, element) {
+  function initializeRelayDestroy () {
+    $('.js-sortable__main-container').each(function (i, el) {
+      $(el).find('.js-sortable__element').each(function (index, element) {
         var fieldId = $(element).find('.fields__container').data('field-id')
         var destroyButton = $('.js-related-field__destroy.is-first')
         var href = destroyButton.attr('href')
         var newHref = href + '?id=' + fieldId
         var cloneButton = destroyButton.clone(true)
         $(cloneButton).removeClass('is-first').attr('href', newHref)
-
         $(element).find('.c-sortable__handle').after(cloneButton)
+        // listen onclick to save current status
+        $(cloneButton).click(function () {
+          _relaySubmitUpdateBeforeAction()
+        })
       })
     })
+  }
+
+  function _relaySubmitUpdateBeforeAction () {
+    var form = $('.inputs__form')
+    if (!$(form).attr('data-is-autosaving') || $(form).attr('data-is-autosaving') == 'false') {
+      $(form).attr('data-remote', true)
+      $(form).attr('data-is-autosaving', true)
+      $(form).submit()
+      $(form).attr('data-remote', false)
+      $(form).attr('data-is-autosaving', false)
+    }
   }
 
   // Init:
 
   function init () {
     initializeRelayNewButton()
-    appendDestroy()
+    initializeRelayDestroy()
   }
 
   return {
